aff814a6e996baf390218a3ea8c8e8da0d633a34
ml-5600 <miguel.lepiz@gmail.com>
2024-11-21 09:50:53 +0000

    Add files via upload

---
+++ SDL_Weather_App.py
@@ -0,0 +1,46 @@
+import json
+from opencage.geocoder import OpenCageGeocode
+import requests
+import sdl2
+import sdl2.ext
+
+class Forecast:
+    def get_zip_code():
+        print("Enter a 5-digit ZIP code:")
+        zip_code = input()
+        while not (len(zip_code) == 5 and zip_code.isdigit()):
+            print("Enter a 5-digit US ZIP code:")
+            zip_code = input()
+        return zip_code
+
+    def get_forecast():
+        key = "cb8e807defef46ceb86f16b64365577b"
+        geocoder = OpenCageGeocode(key)
+        query = Forecast.get_zip_code()
+        
+        # Forward geocode the ZIP code to retrieve coordinates
+        results = geocoder.geocode(query, no_annotations=1, countrycode='us')
+        latitude = results[0]["geometry"]["lat"]
+        longitude = results[0]["geometry"]["lng"]
+
+        url = f"https://api.weather.gov/points/{latitude},{longitude}"
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            forecast_data = response.json()
+            forecast_url = forecast_data['properties']['forecast']
+            forecast_response = requests.get(forecast_url)
+
+        if forecast_response.status_code == 200:
+            forecast_data = forecast_response.json()
+            return forecast_data
+
+    def display_forecast(forecast):
+        pass
+
+def main():
+    forecast = Forecast
+    forecast.display_forecast(forecast.get_forecast())
+
+if __name__ == "__main__" :
+    main()
